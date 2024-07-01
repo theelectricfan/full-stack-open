d17bb281ba276e7a9c5bb4817d974a3450f6f64f
Akshat Singh Raghuwanshi <96365527+theelectricfan@users.noreply.github.com>
2024-07-01 10:20:18 +0530

    complete course-information exercise

---
+++ part2/course-information/src/components/sub_components.jsx
@@ -0,0 +1,40 @@
+const Header = ({ course }) => {
+	return <h1>{course}</h1>;
+};
+
+const Part = ({ part }) => {
+	return (
+		<p>
+			{part.name} {part.exercises}
+		</p>
+	);
+};
+const Content = ({ parts }) => {
+	return (
+		<div>
+			{parts.map((part) => (
+				<Part key={part.id} part={part} />
+			))}
+		</div>
+	);
+};
+
+const Total = ({ parts }) => {
+	return (
+		<b>
+			Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}
+		</b>
+	);
+};
+
+const Course = ({ course }) => {
+	return (
+		<div>
+			<Header course={course.name} />
+			<Content parts={course.parts} />
+			<Total parts={course.parts} />
+		</div>
+	);
+};
+
+export default Course;
