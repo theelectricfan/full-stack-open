f5f7ecc47fd8b2658d0d099b9057b07747d23fe0
Akshat Singh Raghuwanshi <96365527+theelectricfan@users.noreply.github.com>
2024-07-12 14:15:19 +0530

    complete full stack phone application

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(express.static("build"));
+
 app.use(morgan(function(tokens, req, res) {
     return [
         tokens.method(req, res),
